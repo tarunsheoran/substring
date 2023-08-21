542ce6af90f699f9cf5f5756323919e56e58ebd6
tarunsheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-08-21 09:38:20 +0530

    Add files via upload

---
+++ substring.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        if(s.size() == 0) return false;
+        string ss = s + s;
+        return ss.substr(1, ss.size()-2).find(s)!=string::npos;
+    }
+};
